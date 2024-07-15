e14f35496737d8fc50c4875dd94138b7af2bccd7
roomaisa2468 <varishauddin763@gmail.com>
2024-07-15 14:24:47 +0500

    commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// alert("hello");
+// var friuts =prompt("world");
+
+// for loops
+
+// for(let i = 0; i < 20; i++){
+//    document.write("hello world");
+// }
+
+// // for(let i = 14; i < 141; i = i + 14){
+// //     console.log(i);
+// // } 
+
+// for(let i = 24; i < 241; i = i + 24){
+//     console.log(i);
+// }
+
+// let cleancities = ["karachi", "lahore", "islambad", "graet falls" ];
+// let citytocheck = "graet falls";
+
+// for(let i = 0; i <= 4; i++) {
+//     if (citytocheck === cleancities 3) {
+//      alert "it is one of the clean cities"
+//     }
+// }
+
+// let months = ["january", "febuarary", "march", "april"];
+
+// let checktomonth = "april";
+
+// for (let i = 0; i <=4; i++) {
+//     if(checktomonth === months [2]);
+
+//     alert ("one of my fav month");
+
+// } else{     "o"
+
+
+
+// let qualificatinofpakistan = ["SSC", "HSC", "BSC","BS" ,"MS","M.phil" , "PHD"];
+//  document. write("<h3>qualificatinofpakistan</h3>");
+
+//  document.write("<ol>");
+
+//  for(let i = 0; i<=7;i++){
+
+  document.write("</ol>");
+
+  let fruits = ["apple", "mango","banana", "orange", "grapes"];
+ document.write("<h2> fruits </h2>");
+
+ for(let i = 0; i<=5;i++){
+    document.write("<li> <b>" + fruits  [i] + "</li> </b>")
+ }
+
+
+
+let colors = ["black", "purple","yellow", "pink", "blue"];
+ document.write("original" , colors   )
+
